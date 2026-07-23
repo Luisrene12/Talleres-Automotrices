9a7879601dc863a34829e0184aa4a28087c33bf5
moisesmoreno <moisesmoreno.ai@gmail.com>
2026-07-22 21:25:31 -0400

    Guardando avances locales antes de actualizar

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations.hola soymoi
      */
     public function up(): void
     {
